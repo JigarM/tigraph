23e928fb9fcec556cfa066a6054ecf499cf01553
m1ga <miga_miga@gmx.de>
2013-10-03 11:17:51 +0200

    transparent background display text

--- android/example/app.js
+++ android/example/app.js
@@ -6,7 +6,7 @@ var g = require("miga.tigraph");
 
 var points = [[0, 0], [10, 5], [20, 25], [30, 5]];
 var blob = g.drawLines({
-    width : 400, height : 200, maxY : 50, circles : true, circleRadius : 5, circleColor : "#ff0000", backgroundColor : "#ffffff", color : "#000000", lineWidth : 2, points : points
+    height : 200, maxY : 50, text : true, textColor : "#444444", circles : true, circleRadius : 5, circleColor : "#ff0000", backgroundColor : "#ffffff", color : "#000000", lineWidth : 2, points : points
 });
 
 var img = Ti.UI.createImageView({
--- android/src/miga/tigraph/TigraphModule.java
+++ android/src/miga/tigraph/TigraphModule.java
@@ -15,53 +15,63 @@ import android.graphics.Canvas;
 import org.appcelerator.kroll.KrollDict;
 import java.util.HashMap;
 import java.util.Arrays;
+import android.util.DisplayMetrics;
 
 @Kroll.module(name="Tigraph", id="miga.tigraph")
 public class TigraphModule extends KrollModule{
 
 	Context context;
+	int width;
+	int height;
 	
 	public TigraphModule() {
 		super();
 		TiApplication appContext = TiApplication.getInstance();
 		Activity activity = appContext.getCurrentActivity();
 		context=activity.getApplicationContext();
+		DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+		width = metrics.widthPixels;
+		height = metrics.heightPixels;
 		
 	}
 
 	@Kroll.method
 	public TiBlob drawLines(HashMap args){
 	  KrollDict arg = new KrollDict(args);
-	  int w = arg.getInt("width");
+	  
 	  int h = arg.getInt("height");
 	  int lw = arg.getInt("lineWidth");
-	  String bg = arg.optString("backgroundColor", "#FFFFFF");
+	  String bg = arg.optString("backgroundColor", "");
 	  String col = arg.optString("color", "#000000");
 	  String circleCol = arg.optString("circleColor", "#000000");
 	  boolean circle = arg.optBoolean("circles", false);
+	  boolean text = arg.optBoolean("text", false);
+	  String textCol = arg.optString("textColor", "#000000");
 	  int circleRad = arg.optInt("circleRadius", 4);
 	  int maxY = arg.optInt("maxY", h);
+	  int maxX = 0;
 	  
-	  //KrollDict points = arg.getKrollDict("points");
 	  Object[] argArray = (Object[])args.get("points");
 	  
+	  for (int index=0; index < argArray.length-1; index++) {
+	    Object[] pos2 = (Object[])argArray[index+1];
+	    int x2 = Integer.parseInt(pos2[0].toString());
+	    if (x2>maxX) maxX = x2;
+	  }
+	  if (circle) maxX += circleRad*2;
+	  Bitmap resultBitmap = Bitmap.createBitmap(maxX,h, Bitmap.Config.ARGB_8888);
 	  
-	  
-	  
-	  Bitmap resultBitmap = Bitmap.createBitmap(w,h, Bitmap.Config.ARGB_8888);
 	  Canvas canvas = new Canvas(resultBitmap);		
 	  Paint paint = new Paint();
-	  // fill background
-	  canvas.drawColor(Color.parseColor(bg));
+	  
+	  if (bg!=""){
+	    // fill background
+	    canvas.drawColor(Color.parseColor(bg));
+	  }
 	  paint.setAntiAlias(true);
-	  paint.setStrokeWidth(1);
-	  paint.setColor(0xAAAAAAFF);
-	  Log.e("LOG:"  , String.valueOf(h*0.5f));
-	  canvas.drawLine(0,h*0.5f,w,h*0.5f, paint);
 	  
 	  paint.setStrokeWidth(lw);
-	  
-	  
+	  paint.setTextSize(16); 
 	  
 	  for (int index=0; index < argArray.length-1; index++) {
              Object[] pos = (Object[])argArray[index];
@@ -71,25 +81,38 @@ public class TigraphModule extends KrollModule{
 	      float y1 = h- ( Float.parseFloat(pos[1].toString()) /maxY * h);
 	      float x2 = Integer.parseInt(pos2[0].toString());
 	      float y2 =  h- ( Float.parseFloat(pos2[1].toString()) /maxY * h);
-	     	  
+	     
 	     paint.setColor(Color.parseColor(col));
 	     canvas.drawLine(x1,y1,x2,y2, paint);
-	    
 	  }
 	  
 	  if (circle){
-	    for (int index=0; index < argArray.length-1; index++) {
+	    for (int index=0; index < argArray.length; index++) {
 	      Object[] pos = (Object[])argArray[index];
-	      Object[] pos2 = (Object[])argArray[index+1];
-	      
 	      float x1 = Integer.parseInt(pos[0].toString());
 	      float y1 = h- ( Float.parseFloat(pos[1].toString()) /maxY * h);
-	      float x2 = Integer.parseInt(pos2[0].toString());
-	      float y2 =  h- ( Float.parseFloat(pos2[1].toString()) /maxY * h);
+	      
+	      float tw = paint.measureText(pos[1].toString());
 	      paint.setColor(Color.parseColor(circleCol));
-	      canvas.drawCircle(x2,y2, circleRad, paint);
+	      canvas.drawText(pos[1].toString(), x1-tw*0.5f, y1-circleRad-5, paint); 
+	      
+	      paint.setColor(Color.parseColor(circleCol));
+	      canvas.drawCircle(x1,y1, circleRad, paint);
 	    }
 	  }
+	  
+	  if (text){
+	    for (int index=0; index < argArray.length; index++) {
+	      Object[] pos = (Object[])argArray[index];
+	      float x1 = Integer.parseInt(pos[0].toString());
+	      float y1 = h- ( Float.parseFloat(pos[1].toString()) /maxY * h);
+	      
+	      float tw = paint.measureText(pos[1].toString());
+	      paint.setColor(Color.parseColor(textCol));
+	      canvas.drawText(pos[1].toString(), x1-tw*0.5f, y1-circleRad-5, paint); 
+	      }
+	  }
+
 	  
 	  canvas.drawBitmap(resultBitmap, 0,0, paint);
 	  
